dd224ed8bea954536b4d1ac4bcef3a72f2183382
Vy Tran <tuanvy.tran@nfq.asia>
2020-06-17 19:21:46 +0700

    Init jenkins jobs

---
+++ labs/demo-jenkins.groovy
@@ -0,0 +1,38 @@
+def COLOR_MAP = ['SUCCESS': 'good', 'FAILURE': 'danger', 'UNSTABLE': 'danger', 'ABORTED': 'danger']
+pipeline {
+  agent any
+
+//  triggers {
+//    cron('*/5 * * * *')
+//  }
+
+  options {
+    ansiColor('xterm')
+    timestamps()
+    buildDiscarder(logRotator(daysToKeepStr: '5'))
+  }
+
+  environment {
+    ANSIBLE_HOST_KEY_CHECKING = 'False'
+    ANSIBLE = '/usr/bin/ansible'
+  }
+
+  stages{
+    stage("Run ansible command"){
+      steps {
+        sh """
+          ansible-inventory -i host01 --graph
+          ${ANSIBLE}  -i host01 nginx
+            -e ansible_ssh_user=vagrant \
+            -e ansible_ssh_private_key_file=~/.ssh/vagrant.pem \
+            --become  \
+            -m shell \
+            -a "pwd"
+        """
+      }
+    }
+  }
+
+  post {
+  }
+}
